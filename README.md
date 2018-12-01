efb5747183583260e35b56d3012f7940ed4b0c59
Zac Bohon <startswithz@gmail.com>
2018-12-01 04:02:45 -0600

    Day one solution

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # advent_of_code_2018
 Advent of Code 2018 Solutions (https://adventofcode.com/2018)
+
+## Day One
+Part one was pretty easy and natural to code, but part two took a bit more work, and I'm not totally
+happy with how i ended up doing it. I want to refactor to use a `loop` instead of a `while`.
+
+[Solution](day_one/solution.clj)
---
+++ day_one/solution.clj
@@ -0,0 +1,23 @@
+(defn now [] (java.util.Date.))
+
+; Part One
+(def input (->>
+    (slurp "day_one/input.txt")
+    clojure.string/split-lines
+    (map read-string)))
+
+(println "Solution One (" (now) "):" (reduce + input))
+
+; Part Two
+(defn solve [arr]
+        (def seen [])
+        (def i 0)
+        (def freq 0)
+        (while (not (some #(= freq %) seen)) (do
+            (def seen (conj seen freq))
+            (def freq (+ (last seen) (nth arr i)))
+            (def i (mod (inc i) (count arr)))
+        ))
+        freq)
+
+(println "Solution Two(" (now) "):"  (solve input))
